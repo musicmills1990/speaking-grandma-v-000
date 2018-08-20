8a848579e7f9ca891224badcfff2264214e9a877
Marc Mills <mdmills1990@gmail.com>
2018-08-20 14:46:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,12 @@
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+    
   elsif phrase == phrase.downcase || phrase.capitalize
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  
   end
 end
